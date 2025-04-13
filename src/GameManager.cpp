1aee6e7ae06c83dd1ce291bb341afe9b802e3e26
anthonyvttran <anthonytran1429@gmail.com>
2025-04-12 17:50:23 -0700

    did the gamemanager stuff

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,17 +1,38 @@
-#include <Arduino.h>
 #include "GameManager.h"
+#include "Audio.h"
+#include <chrono>
+#include <thread>
 
 GameManager::GameManager()
 {
     // Setup all game variables
-    this->lives = 3;
-    this->speed = 1.0;
-    this->size = 10;
+    this->lives = 3;    //3 lives
+    this->speed = 1.0;  //default speed
+    this->size = 10;    //size is 10? idk what that means
+
+    this->enemyLife = 1;
+    this->enemySize = 10;
 }
 
 void GameManager::initializeGame()
 {
     // Initialize anything if necessary, if not then delete this function
+
+    //keep extremely simple. Do 1 enemy first. figure out array later
+    //add enemy variables
+    
+    enemyX = -0.8;
+    enemyY = 0.8;
+
+    
+
+    //no blockades
+    //player intializes in the middle 
+
+    playerX = 0;   //start the player in the middle, ten bits up about
+    playerY = -0.5;
+
+
 }
 
 void GameManager::gameLoop()
@@ -19,6 +40,31 @@ void GameManager::gameLoop()
     /* Runs in the microcontroller's loop function.
     Handles any game logic that needs to update continuously such as player and enemy movement
     */ 
+
+    //what loops?
+    //the movement of the enemies needs to loop
+    //every second (add timescale) enemies move to the right and reset
+
+    while (enemyX < 0.8) {
+        enemyX += 0.2;
+        sleep(1);   //adds a delay of 1 sec before it continues moving again
+    }
+
+    if (enemyX >= 0.8) {
+        enemyX = -0.8;
+        enemyY -= 0.2;
+    }
+
+    if (enemyY < -0.4) {
+        this->lives -= 1;
+        enemyX = -0.8;
+        enemyY = 0.8;
+    }
+
+
+
+
+
 
 }
 
@@ -32,4 +78,15 @@ void GameManager::handleInput(float x, float y, int sw, float delta)
     angle = atan2f(y, x);
     playerX += speed * x * delta;
     playerY += speed * y * delta;
+
+
+    //we also need the shooting mechanics
+    if (sw = 1) {
+        //idk how to do shooting
+    }
+    
+    //player cannot go above -0.4 Y
+    if (playerY > -0.4) {
+        playerY = -0.4; //should always "reset" to -0.4
+    }
 }
