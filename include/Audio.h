0186c9de62582e04652a6de274da31af23ed7d35
anthonyvttran <anthonytran1429@gmail.com>
2025-04-12 19:59:42 -0700

    updated audio.h

--- include/Audio.h
+++ include/Audio.h
@@ -11,7 +11,6 @@ DFRobotDFPlayerMini player; //initializing the object
 void audioInit() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  //Serial.println("balls");
 
 
   //no idea what the first 2 inputs mean
@@ -19,22 +18,39 @@ void audioInit() {
   mySerial1.begin(9600, SERIAL_8N1, rxPin, txPin);
 
   delay(1000); //needed to apparently setup the board
+}
 
-void AudioInit()
+void AudioInit() {
+    player.play(1);
+}
 
 void playerShoot() {
-    player.play(playerShoot);
+    player.play(5);
 }
 
 void playerHit() {
-    player.play(playerHit);
+    player.play(3);
 }
 
-void playerDies()) {
-    player.play(playerDies);
+void playerDies() {
+    player.play(6);
 }
 
-void 
-
-
+void playerMoves() {
+    player.play(4);
 }
+
+void enemyMoves() {
+    player.play(8);
+}
+
+void enemyShoots() {
+    player.play(7);
+}
+
+void enemyHit() {
+    player.play(2);
+}
+
+
+
