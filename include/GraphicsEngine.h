9d86e15c0e3ab349e9e3a726fedeb7d9350ba417
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2025-04-12 18:17:29 -0700

    Simplify project scope for graphics

--- include/GraphicsEngine.h
+++ include/GraphicsEngine.h
@@ -3,8 +3,8 @@
 
 namespace Graphics
 {
-    void drawPlayer(float x, float y, float angle, Adafruit_SSD1327* display);
     void drawEnemy(float x, float y, float z, Adafruit_SSD1327* display);
+    void drawEnemies(float x[], float y[], float z[], Adafruit_SSD1327* display);
     const float drawDistance = 32.0f;
 }
 
--- src/GraphicsEngine.cpp
+++ src/GraphicsEngine.cpp
@@ -1,28 +1,18 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1327.h>
-#include <ArduinoEigen.h>
 #include "GraphicsEngine.h"
 
 using namespace Graphics;
-using Eigen::Matrix3f;
-
-void drawPlayer(float x, float y, float angle, Adafruit_SSD1327* display)
-{
-    Matrix3f triangleVertices = 
-    (Matrix3f() << 
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    ).finished();
-
-    Eigen::Transform<float, 3, Eigen::Affine> t;
-    t.translate(Eigen::Vector3f(x, y, 0));
-    t.rotate(Eigen::AngleAxisf(angle, Eigen::Vector3f(0, 0, 1)));
-
-    display->drawTriangle(0, 40, -40, -40, 40, -40, SSD1327_WHITE);
-}
 
 void Graphics::drawEnemy(float x, float y, float z, Adafruit_SSD1327* display)
 {
     display->drawCircle(x * display->width(), y * display->height(), z * drawDistance, SSD1327_WHITE);
+}
+
+void Graphics::drawEnemies(float x[], float y[], float z[], Adafruit_SSD1327* display)
+{
+    for (int i = 0; i < sizeof(x)/sizeof(x[0]); i++)
+    {
+        drawEnemies(&x[i], &y[i], &z[i], display);
+    }
 }
